b43ee22622f18e37aed1a7a2337abad1daeab39d
25020335-Quan <25020335@vnu.edu.vn>
2026-04-20 00:25:17 +0700

    Application file

--- src/main/java/auction/App.java
+++ src/main/java/auction/App.java
@@ -1,13 +1,26 @@
 package auction;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import auction.model.*;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        // 1. Lấy instance duy nhất của AuctionManager (Singleton)
+        AuctionManager manager = AuctionManager.getInstance();
+
+        Bidder userA = new Bidder("U01", "25020335-1", "password123");
+        Bidder userB = new Bidder("U02", "25020335-2", "password456");
+        Item electronics = ItemFactory.createItem("ELECTRONICS", "I01", "Laptop Gaming", 1500.0);
+
+        // Thực hiện đặt giá
+        electronics.placeBid(userA, 1600.0); // Thành công -> Tạo transaction
+        electronics.placeBid(userB, 1700.0); // Thất bại -> Không tạo transaction
+
+        List<BidTransaction> his = manager.getHistoryByItem("I01");
+        for (BidTransaction trans : his) {
+            System.out.println(trans);
+        }
+        System.out.println(manager.getCurrentMaxPrice("I01"));
     }
 }
