80d79122f3eabfe45335a44fc8fa66e020539e84
25020335-Quan <25020335@vnu.edu.vn>
2026-04-24 18:25:25 +0700

    Member class is a combined version of Bidder and Seller

---
+++ src/main/java/auction/model/Member.java
@@ -0,0 +1,39 @@
+package auction.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Member extends User implements Bidder, Seller {
+    private List<Item> ownedItems = new ArrayList<>();
+
+    public Member(String id, String username, String password) {
+        super(id, username, password);
+    }
+
+    @Override
+    public Boolean bid(Item item, double amount) {
+        return AuctionManager.getInstance().attemptBid(item, id, amount);
+    }
+
+    @Override
+    public void createItem(String type, String id, String name, double startingPrice) {
+        ownedItems.add(ItemFactory.factoryItem(type, this.id, id, name, startingPrice));
+        // this.id là ownerId
+    }
+
+    @Override
+    public void postItem(Item item) {
+        item.setState(AuctionState.OPEN);
+    }
+
+    public Item findById(String id) {
+        for (Item item : ownedItems) {
+            if (item.getId().equals(id)) {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+}
