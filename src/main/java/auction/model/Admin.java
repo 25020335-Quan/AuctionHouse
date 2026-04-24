af8dca6f97106d796bc300198703fbc60faf3384
25020335-Quan <25020335@vnu.edu.vn>
2026-04-24 18:27:31 +0700

    Admin has yet to have its own methods

--- src/main/java/auction/model/Admin.java
+++ src/main/java/auction/model/Admin.java
@@ -8,9 +8,4 @@ public class Admin extends User {
     public Admin(String id, String username, String password) {
         super(id, username, password);
     }
-
-    @Override
-    public void displayRole() {
-        System.out.println("[Vai trò] Quản trị viên: Tôi quản lý toàn bộ hệ thống và xử lý tranh chấp.");
-    }
 }
