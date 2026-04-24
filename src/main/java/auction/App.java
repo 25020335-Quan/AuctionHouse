1d8f282acd540e58c1cd35e61176e8a7f2c2fac4
25020335-Quan <25020335@vnu.edu.vn>
2026-04-24 18:38:28 +0700

    Trying out new features

--- src/main/java/auction/App.java
+++ src/main/java/auction/App.java
@@ -2,25 +2,16 @@ package auction;
 
 import auction.model.*;
 
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        // 1. Lấy instance duy nhất của AuctionManager (Singleton)
-        AuctionManager manager = AuctionManager.getInstance();
 
-        Bidder userA = new Bidder("U01", "25020335-1", "password123");
-        Bidder userB = new Bidder("U02", "25020335-2", "password456");
-        Item electronics = ItemFactory.createItem("ELECTRONICS", "I01", "Laptop Gaming", 1500.0);
+        Member userA = new Member("U01", "25020335-1", "password123");
+        Member userB = new Member("U02", "25020335-2", "password456");
 
-        // Thực hiện đặt giá
-        electronics.placeBid(userA, 1600.0); // Thành công -> Tạo transaction
-        electronics.placeBid(userB, 1700.0); // Thất bại -> Không tạo transaction
-
-        List<BidTransaction> his = manager.getHistoryByItem("I01");
-        for (BidTransaction trans : his) {
-            System.out.println(trans);
-        }
-        System.out.println(manager.getCurrentMaxPrice("I01"));
+        userA.createItem("ELECTRONICS", "E01", "Laptop Gaming", 1000);
+        userB.bid(userA.findById("E01"), 1100); // Error because item is still pending
+        userA.postItem(userA.findById("E01"));
+        userA.bid(userA.findById("E01"), 1200); // Can not bid your own item
+        userB.bid(userA.findById("E01"), 1500);
     }
 }
