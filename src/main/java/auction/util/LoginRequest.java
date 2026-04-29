ddafa8b079ec99d5994959ee8e4fbdd70ff72a33
25020335-Quan <25020335@vnu.edu.vn>
2026-04-29 23:18:34 +0700

    added new object to wrap login credentials

---
+++ src/main/java/auction/util/LoginRequest.java
@@ -0,0 +1,16 @@
+package auction.util;
+
+import java.io.Serializable;
+
+public class LoginRequest implements Serializable {
+    private String username;
+    private String password;
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    // Getter cho username và password
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+}
