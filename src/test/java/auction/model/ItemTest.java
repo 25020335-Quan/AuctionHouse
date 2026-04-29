7bc74dc8bc2bc3d5dc39dcf414b3aa7668da0244
25020335-Quan <25020335@vnu.edu.vn>
2026-04-29 18:31:33 +0700

    test features to be added

--- src/test/java/auction/model/ItemTest.java
+++ src/test/java/auction/model/ItemTest.java
@@ -15,17 +15,5 @@ public class ItemTest {
         testMember.postItem(testItem);
     }
 
-    @Test
-    void testPlaceBidHigherThanCurrent() {
-        boolean result = testMember.bid(testItem, 1200.0);
-        assertTrue(result, "Đặt giá cao hơn phải thành công.");
-        assertEquals(1200.0, testItem.getCurrentPrice(), "Giá hiện tại phải cập nhật lên 1200.");
-    }
 
-    @Test
-    void testPlaceBidLowerThanCurrent() {
-        boolean result = testMember.bid(testItem, 900.0);
-        assertFalse(result, "Đặt giá thấp hơn phải thất bại.");
-        assertEquals(1000.0, testItem.getCurrentPrice(), "Giá hiện tại phải giữ nguyên 1000.");
-    }
 }
