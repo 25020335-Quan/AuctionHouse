337e033cdc653f57734aad94c8efd06b6b071cb1
25020335-Quan <25020335@vnu.edu.vn>
2026-05-06 13:53:31 +0700

    added task to additem and notifications

--- src/main/java/auction/controller/AddItemController.java
+++ src/main/java/auction/controller/AddItemController.java
@@ -1,13 +1,20 @@
 package auction.controller;
 
+import auction.client.AuctionClient;
 import auction.model.AuctionManager;
-import auction.model.factory.FactoryProvider;
 import auction.model.item.Art;
 import auction.model.item.Electronics;
 import auction.model.item.Item;
 import auction.model.item.Vehicle;
+import auction.model.users.User;
+import auction.util.AddItemRequest;
+import auction.util.LoginRequest;
+import auction.util.NotificationRequest;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -37,19 +44,49 @@ public class AddItemController {
         String itemId = "I" + System.currentTimeMillis();
         String ownerId = parentController.getCurrentUser().getId();
         String typeInput  = typeField.getText().trim().toLowerCase();
-//        if (typeInput.contains("art")) {
-//            newItem = new Art(itemId, ownerId, name, price);
-//        } else if (typeInput.contains("electronics")) {
-//            newItem = new Electronics(itemId, ownerId, name, price);
-//        } else if (typeInput.contains("vehicle")) {
-//            newItem = new Vehicle(itemId, ownerId, name, price);
-//        } else {
-//            // Mặc định hoặc báo lỗi nếu nhập sai loại
-//            System.out.println("Loại sản phẩm không hợp lệ");
-//        }
-        Item newItem = FactoryProvider.createItemByType(typeInput, itemId, ownerId, name, price);
-        AuctionManager.getInstance().addItem(newItem);
-        parentController.refreshUI();
-        ((Stage)nameField.getScene().getWindow()).close();
+        if (typeInput.contains("art")) {
+            newItem = new Art(itemId, ownerId, name, price);
+        } else if (typeInput.contains("electronics")) {
+            newItem = new Electronics(itemId, ownerId, name, price);
+        } else if (typeInput.contains("vehicle")) {
+            newItem = new Vehicle(itemId, ownerId, name, price);
+        } else {
+            // Mặc định hoặc báo lỗi nếu nhập sai loại
+            System.out.println("Loại sản phẩm không hợp lệ");
+        }
+
+        Task<Item> addItemTask = new Task<Item>() {
+            @Override
+            protected Item call() throws Exception {
+                while (true) {
+                    Object response = AuctionClient.getInstance().sendRequest(new AddItemRequest(newItem));
+
+                    if (response instanceof Item) {
+                        return (Item) response; // Chỉ thoát vòng lặp khi nhận đúng Item
+                    } else if (response instanceof NotificationRequest note) {
+                        // Tiện thể xử lý luôn thông báo nếu nhận nhầm
+                        Platform.runLater(() -> showAlert(note.getMsg()));
+                        // Sau đó tiếp tục vòng lặp để đợi gói tin tiếp theo (Hy vọng là Item)
+                    }
+                }
+            }
+        };
+
+        addItemTask.setOnSucceeded(e -> {
+            AuctionManager.getInstance().addItem(newItem);
+            parentController.refreshUI();
+            ((Stage)nameField.getScene().getWindow()).close();
+            System.out.println("Đã thêm sản phẩm thành công!");
+        });
+        Thread thread = new Thread(addItemTask);
+        thread.start();
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION); // Loại thông báo có icon chữ 'i'
+        alert.setTitle("Thông báo hệ thống");
+        alert.setHeaderText(null); // Không dùng tiêu đề phụ
+        alert.setContentText(message);
+        alert.showAndWait(); // Hiển thị và bắt người dùng nhấn OK mới được làm tiếp
     }
 }
