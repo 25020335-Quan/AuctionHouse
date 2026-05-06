148ee2506266e15af0a2391a47eb88aef41929a3
25020335-Quan <25020335@vnu.edu.vn>
2026-05-06 13:54:18 +0700

    added broadcast

--- src/main/java/auction/server/AuctionServer.java
+++ src/main/java/auction/server/AuctionServer.java
@@ -2,10 +2,12 @@ package auction.server;
 
 import java.io.*;
 import java.net.*;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class AuctionServer {
     private static final int PORT = 1234;
-
+    private static List<ClientHandler> clients = new CopyOnWriteArrayList<>();
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server đang chạy tại cổng " + PORT + "...");
@@ -14,12 +16,22 @@ public class AuctionServer {
                 // Chấp nhận kết nối từ Client
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Có Client mới kết nối: " + clientSocket.getInetAddress());
-
+                ClientHandler handler = new ClientHandler(clientSocket);
                 // Tạo luồng riêng để xử lý Client này (Multi-threading)
-                new ClientHandler(clientSocket).start();
+                clients.add(handler);
+                handler.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public static void broadcast(Object message) {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+        }
+    }
+
+    public static void removeClient(ClientHandler handler) {
+        clients.remove(handler);
     }
 }
