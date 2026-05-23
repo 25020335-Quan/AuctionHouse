44dcd565f3522af97fe9f12125deed147e40a25d
25020335-Quan <25020335@vnu.edu.vn>
2026-05-23 14:33:28 +0700

    updated for online database

--- src/main/java/auction/model/service/DBConnection.java
+++ src/main/java/auction/model/service/DBConnection.java
@@ -5,11 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/auction_house?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root"; // Mặc định của XAMPP
-    private static final String PASS = "";     // Mặc định của XAMPP là để trống
+    // Mặc định của XAMPP là để trống
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+        String host = "bjsa1yg2p28c1jctzcft-mysql.services.clever-cloud.com";
+        String database = "bjsa1yg2p28c1jctzcft";
+        String user = "udappkqij1y85myz";
+        String password = "xR89VlU5JKGhvSsFOG97";
+
+        String url = "jdbc:mysql://" + host + ":3306/" + database + "?useSSL=false&allowPublicKeyRetrieval=true";
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        // Dòng này xác nhận trạng thái kết nối thành công
+//        System.out.println("[Database] Kết nối thành công tới Clever Cloud DB: " + database);
+
+        return conn;
     }
 }
