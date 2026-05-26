7cecf1f17b11daaa5fda5b15da97fd0fba67e8d4
25020427-HoangVietNguyen <25020427@vnu.edu.vn>
2026-05-26 21:24:40 +0700

    created mock (test) item

--- src/main/java/auction/server/AuctionServer.java
+++ src/main/java/auction/server/AuctionServer.java
@@ -1,7 +1,13 @@
 package auction.server;
 
+import auction.model.AuctionManager;
+import auction.model.item.Art;
+import auction.model.item.Item;
+import auction.model.state.AuctionState;
+
 import java.io.*;
 import java.net.*;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -9,6 +15,30 @@ public class AuctionServer {
     private static final int PORT = 1234;
     private static List<ClientHandler> clients = new CopyOnWriteArrayList<>();
     public void start() {
+        try {
+            auction.model.service.DatabaseService dbService = new auction.model.service.DatabaseService();
+            dbService.loadAllItemsToManager();
+            System.out.println("[Server] Đã tải toàn bộ dữ liệu từ Database vào kho tổng!");
+        } catch (Exception e) {
+            System.out.println("Lỗi load Database: " + e.getMessage());
+        }
+        try {
+            Item mockItem = new Art("TEST-999", "363636", "Đồng hồ Rolex (Test)", 100000);
+            mockItem.setStartTime(LocalDateTime.now());
+
+            mockItem.setEndTime(LocalDateTime.now().plusSeconds(120));
+            mockItem.setState(AuctionState.RUNNING);
+
+            // Bơm vào kho lưu trữ trung tâm của Server
+            AuctionManager.getInstance().addItem(mockItem);
+            System.out.println("[Server] Đã khởi tạo thành công sản phẩm test: TEST-999");
+        } catch (Exception e) {
+            System.out.println("Lỗi tạo đồ test: " + e.getMessage());
+        }
+        // =========================================================
+
+
+
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server đang chạy tại cổng " + PORT + "...");
 
