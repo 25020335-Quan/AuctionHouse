de18d30439f3effde33095a700c2c5b1ae9c123d
25020427-HoangVietNguyen <25020427@vnu.edu.vn>
2026-05-26 21:38:21 +0700

    Optimize DB and complete online auction flow.

--- src/main/java/auction/server/ClientHandler.java
+++ src/main/java/auction/server/ClientHandler.java
@@ -3,17 +3,17 @@ package auction.server;
 import auction.model.AuctionManager;
 import auction.model.item.Item;
 import auction.model.service.DatabaseService;
+import auction.model.transaction.BidTransaction;
 import auction.model.users.User;
-import auction.util.AddItemRequest;
-import auction.util.GetItemListRequest;
-import auction.util.LoginRequest;
-import auction.util.NotificationRequest;
+import auction.util.*;
+import auction.model.notification.BidNotification;
 
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// Kế thừa Thread chạy song song
 public class ClientHandler extends Thread {
     private Socket socket;
     private ObjectInputStream in;
@@ -30,46 +30,76 @@ public class ClientHandler extends Thread {
         try {
             out = new ObjectOutputStream(socket.getOutputStream());
             in = new ObjectInputStream(socket.getInputStream());
+
             while (true) {
                 // Đọc đối tượng yêu cầu từ Client (Serialization)
                 Object request = in.readObject();
 
-                // Logic xử lý (Ví dụ: Kiểm tra đăng nhập hoặc Đấu giá)
+                // Logic xử lý
                 if (request instanceof LoginRequest loginData) {
                     User user = dbService.checkLogin(loginData.getUsername(), loginData.getPassword());
-                    out.writeObject(user);
+                    this.sendMessage(user);
+
                 } else if (request instanceof AddItemRequest itemData) {
                     AuctionServer.broadcast(new NotificationRequest("Có sản phẩm mới: " + itemData.getItem().getName()));
                     dbService.addItem(itemData.getItem());
-                    System.out.println("Server: Đang xử lý thêm đồ: " + itemData.getItem().getName());;
-                    out.writeObject(itemData.getItem());
-                    out.flush();
+                    System.out.println("Server: Đang xử lý thêm đồ: " + itemData.getItem().getName());
+                    this.sendMessage(itemData.getItem());
 
                 } else if (request instanceof GetItemListRequest) {
-                    // 1. Server gọi hàm đọc từ DB vào AuctionManager (chỉ máy Server mới làm việc này)
-                    dbService.loadAllItemsToManager();
-
-                    // 2. Lấy danh sách từ Manager
+                    // Lấy kho mới nhất gửi về Client
                     List<Item> currentItems = AuctionManager.getInstance().getAllItems();
+                    this.sendMessage(new ArrayList<>(currentItems));
 
-                    // 3. Gửi danh sách này ngược lại cho Client
-                    // Gói vào ArrayList để đảm bảo Serializable hoạt động tốt
-                    out.writeObject(new ArrayList<>(currentItems));
-                    out.flush();
+                } else if (request instanceof GetItemHistoryRequest) {
+                    GetItemHistoryRequest req = (GetItemHistoryRequest) request;
+                    List<BidTransaction> history = AuctionManager.getInstance().getHistoryByItem(req.getItemId());
+                    // Đã fix đổi sang sendMessage
+                    this.sendMessage(new java.util.ArrayList<>(history));
+
+                } else if (request instanceof auction.util.BidRequest bidReq) {
+                    try {
+                        Item item = null;
+                        for (Item i : AuctionManager.getInstance().getAllItems()) {
+                            if (i.getId().equals(bidReq.getItemId())) {
+                                item = i;
+                                break;
+                            }
+                        }
+
+                        if (item != null) {
+                            // Chạy đấu giá
+                            AuctionManager.getInstance().attemptBid(item, bidReq.getBidderId(), bidReq.getAmount());
+
+                            // Thông báo cho các client khác
+                            BidNotification notify = new BidNotification(
+                                    item.getId(),
+                                    bidReq.getBidderId(),
+                                    bidReq.getAmount(),
+                                    item.getEndTime()
+                            );
+                            auction.server.AuctionServer.broadcast(notify);
+
+                            // Trả về báo cáo thành công
+                            this.sendMessage("SUCCESS");
+                        } else {
+                            this.sendMessage("ERROR: Product not found!");
+                        }
+                    } catch (auction.exception.InvalidBidException e) {
+                        this.sendMessage("ERROR:" + e.getMessage());
+                    }
                 }
-
-                // Gửi kết quả về Client
-                out.flush();
             }
         } catch (Exception e) {
             System.out.println("Client đã ngắt kết nối.");
         }
     }
 
-    public void sendMessage(Object message) {
+    public synchronized void sendMessage(Object message) {
         try {
             out.writeObject(message);
             out.flush();
+            out.reset();
         } catch (IOException e) {
             AuctionServer.removeClient(this); // Xóa nếu client ngắt kết nối
         }
