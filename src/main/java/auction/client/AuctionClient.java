fe905fb6f2dbbe393d1c8cc86288605c477fa7a5
25020427-HoangVietNguyen <25020427@vnu.edu.vn>
2026-05-27 10:40:13 +0700

    added notification

--- src/main/java/auction/client/AuctionClient.java
+++ src/main/java/auction/client/AuctionClient.java
@@ -35,7 +35,6 @@ public class AuctionClient {
 
                         // Trạng thái A: Nhận được tin báo có người Bid (Realtime)
                         if (response instanceof BidNotification notify) {
-
                             // Cập nhật giá mới cho món đồ trong kho Client
                             for (auction.model.item.Item i : auction.model.AuctionManager.getInstance().getAllItems()) {
                                 if (i.getId().equals(notify.getItemId())) {
@@ -64,15 +63,43 @@ public class AuctionClient {
                                     // Cập nhật giá và biểu đồ
                                     currentRoom.refreshPriceFromNetwork(notify.getHighestBidderId(), notify.getNewPrice());
                                 }
-                                //Load lại mainscreen
-                                if (mainScreen != null) {
-                                     mainScreen.refreshLocalUI();
+                                // nếu đã đăng nhập và vào được main screen -> xử lý UI
+                                if (mainScreen != null && mainScreen.getCurrentUser() != null) {
+                                    String myUserId = mainScreen.getCurrentUser().getId();
+                                    if (!notify.getHighestBidderId().equals(myUserId)) {
+                                        // Kiểm tra xem mình có đang theo dõi món này không?
+                                        // Mình là chủ món đồ hoặc mình đã từng tham gia bid món này
+                                        boolean isMyItem = checkIsMyItem(notify.getItemId(), myUserId);
+                                        boolean didIBid = checkDidIBid(notify.getItemId(), myUserId);
+                                        if (isMyItem) {
+                                            mainScreen.showToast("Your item: " + notify.getItemId() + "just got a new bid: " + notify.getNewPrice());
+                                            mainScreen.addNotification("Your item: " + notify.getItemId() +" just got a new bid: " + notify.getNewPrice());
+                                        } else if (didIBid) {
+                                            mainScreen.showToast("Outbid! Someone just placed a higher bid of: " + notify.getNewPrice() + "for item " + notify.getItemId() );
+                                            mainScreen.addNotification("Outbid! Someone just placed a higher bid of: " + notify.getNewPrice() + "for item " + notify.getItemId());
+                                        }
+                                    } else {
+                                        String msg = "✅ You have successfully placed a bid of: " + String.format("%,.0f VNĐ", notify.getNewPrice()) + "for item " + notify.getItemId();
+                                        mainScreen.showToast(msg);        // Hiện Toast xanh lá báo thành công
+                                        mainScreen.addNotification(msg); // Lưu vào hộp thư
+                                    }
+                                    //Load lại mainscreen
+                                    if (mainScreen != null) {
+                                        mainScreen.refreshLocalUI();
+                                    }
                                 }
                             });
                         }
                         // Trạng thái B: Thông báo hệ thống
                         else if (response instanceof NotificationRequest notifyReq) {
-                            System.out.println("Hệ thống: " + notifyReq.getMsg());
+                            javafx.application.Platform.runLater(() -> {
+                                if (mainScreen != null) {
+                                    String msg = "🆕 " + notifyReq.getMsg();
+                                    mainScreen.showToast(msg);        // hiện thông báo lướt
+                                    mainScreen.addNotification(msg); // lưu vào hộp thư
+                                    mainScreen.refreshLocalUI();      // load lại danh sách sản phẩm
+                                }
+                            });
                         }
 
                         // Trạng thái C: Là kết quả trả về của một sendRequest bình thường
@@ -128,5 +155,34 @@ public class AuctionClient {
 
     public void setMainScreen(MainScreenController screen) {
         this.mainScreen = screen;
+    }
+
+
+    //Kiểm tra xem món đồ có ID này có phải do chính mình đăng bán không
+    private boolean checkIsMyItem(String itemId, String myUserId) {
+        for (auction.model.item.Item item : auction.model.AuctionManager.getInstance().getAllItems()) {
+            // Tìm đúng món đồ đó trong kho
+            if (item.getId().equals(itemId)) {
+                // So sánh xem ID người bán có trùng với ID của mình không
+                return item.getOwnerId().equals(myUserId);
+            }
+        }
+        return false; // Không tìm thấy đồ hoặc không phải của mình
+    }
+
+    //Kiểm tra xem mình đã từng đặt giá (bid) cho món đồ này lần nào chưa
+    private boolean checkDidIBid(String itemId, String myUserId) {
+        // Lôi toàn bộ lịch sử đấu giá của món đồ này ra
+        java.util.List<auction.model.transaction.BidTransaction> history =
+                auction.model.AuctionManager.getInstance().getHistoryByItem(itemId);
+
+        // Quét từng BidTransaction
+        for (auction.model.transaction.BidTransaction tx : history) {
+            // nếu có lịch sử trùng -> return true
+            if (tx.getBidderId().equals(myUserId)) {
+                return true;
+            }
+        }
+        return false; // Quét hết mà không có
     }
 }
