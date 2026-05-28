b2249af4f3a561c9110b6ec26b74c8d3aa9bbc71
25020335-Quan <25020335@vnu.edu.vn>
2026-05-28 19:54:12 +0700

    added email fields to users

--- src/main/java/auction/model/users/Member.java
+++ src/main/java/auction/model/users/Member.java
@@ -15,8 +15,8 @@ import java.util.ArrayList;
 public class Member extends User implements Bidder, Seller, Serializable {
     private List<Item> ownedItems = new ArrayList<>();
 
-    public Member(String id, String username, String password) {
-        super(id, username, password);
+    public Member(String id, String username, String password, String email) {
+        super(id, username, password, email);
     }
 
     @Override
