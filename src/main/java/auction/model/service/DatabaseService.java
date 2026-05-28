504bcfaedcffad8e255cab0670b9ab3285aa7864
25020335-Quan <25020335@vnu.edu.vn>
2026-05-28 20:28:53 +0700

    added more methods for dbservice

--- src/main/java/auction/model/service/DatabaseService.java
+++ src/main/java/auction/model/service/DatabaseService.java
@@ -130,6 +130,7 @@ public class DatabaseService {
                 System.out.println("[Database] Failed to update. Item ID not found: " + itemId);
                 return false;
             }
+
         } catch (SQLException e) {
             System.err.println("[Database] Error executing update: " + e.getMessage());
             return false;
