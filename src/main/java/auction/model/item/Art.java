eaa8e3582dad74aaad5dd40f19c30400670e0abe
25020427-HoangVietNguyen <25020427@vnu.edu.vn>
2026-05-28 22:22:56 +0700

    added another constructor to generate without id

--- src/main/java/auction/model/item/Art.java
+++ src/main/java/auction/model/item/Art.java
@@ -2,8 +2,12 @@ package auction.model.item;
 
 // Lớp cho tác phẩm nghệ thuật
 public class Art extends Item {
-    public Art(String id, String ownerId, String name, double startingPrice) {
 
+    public Art(String ownerId, String name, double startingPrice) {
+        super(ownerId, name, startingPrice);
+    }
+
+    public Art(String id, String ownerId, String name, double startingPrice) {
         super(id, ownerId, name, startingPrice);
     }
 
--- src/main/java/auction/model/item/Vehicle.java
+++ src/main/java/auction/model/item/Vehicle.java
@@ -2,6 +2,10 @@ package auction.model.item;
 
 // Lớp cho tác phương tiện giao thông
 public class Vehicle extends Item {
+    public Vehicle(String ownerId, String name, double startingPrice) {
+        super(ownerId, name, startingPrice);
+    }
+    
     public Vehicle(String id, String ownerId, String name, double startingPrice) {
         super(id, ownerId, name, startingPrice);
     }
