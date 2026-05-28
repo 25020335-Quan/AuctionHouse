5e4ecbe1eb9d8f90cc75ffb9e8ad7929992047f1
25020427-HoangVietNguyen <25020427@vnu.edu.vn>
2026-05-28 22:29:28 +0700

    added function to the factory provider to create item without id

--- src/main/java/auction/model/factory/FactoryProvider.java
+++ src/main/java/auction/model/factory/FactoryProvider.java
@@ -16,8 +16,18 @@ public class FactoryProvider {
             System.out.println("Loại sản phẩm không hợp lệ");
         }
         if (factory != null) {
-            return factory.createItem(id, ownerId, name, startingPrice);
+            return factory.createItem(id , ownerId, name, startingPrice);
         }
+        return null;
+    }
+
+    public static Item createNewItemByType(String type, String ownerId, String name, double startingPrice) {
+        ItemFactory factory = null;
+        if (type.equalsIgnoreCase("electronics")) factory = new ElectronicsFactory();
+        else if (type.equalsIgnoreCase("art")) factory = new ArtFactory();
+        else if (type.equalsIgnoreCase("vehicle")) factory = new VehicleFactory();
+
+        if (factory != null) return factory.createNewItem(ownerId, name, startingPrice);
         return null;
     }
 }
