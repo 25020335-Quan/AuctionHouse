2c4ff6663f9a480b0759909ef1525a24fcacbcd0
25020427-HoangVietNguyen <25020427@vnu.edu.vn>
2026-05-28 22:25:57 +0700

    added a function to create item without id

--- src/main/java/auction/model/factory/ItemFactory.java
+++ src/main/java/auction/model/factory/ItemFactory.java
@@ -3,5 +3,7 @@ package auction.model.factory;
 import auction.model.item.Item;
 
 public abstract class ItemFactory {
-    public abstract Item createItem(String id, String ownerId, String name, double startingPrice);
+    public abstract Item createItem(String id , String ownerid , String name, double startingPrice);
+
+    public abstract Item createNewItem(String ownerid, String name, double startingPrice);
 }
