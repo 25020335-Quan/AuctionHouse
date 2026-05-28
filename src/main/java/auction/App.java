b6d0871a3312ce215a4e5145fee05343ae7f5809
25020427-HoangVietNguyen <25020427@vnu.edu.vn>
2026-05-28 22:31:15 +0700

    minor changes to the constructor

--- src/main/java/auction/App.java
+++ src/main/java/auction/App.java
@@ -5,8 +5,8 @@ import auction.model.users.Member;
 public class App {
     public static void main(String[] args) {
 
-        Member userA = new Member("U01", "25020335-1", "password123");
-        Member userB = new Member("U02", "25020335-2", "password456");
+        Member userA = new Member("U01", "25020335-1", "password123" ,"abcd@gmail.com");
+        Member userB = new Member("U02", "25020335-2", "password456","abcd@gmail.com");
 
         userA.createItem("ELECTRONICS", "E01", "Laptop Gaming", 1000);
         userB.bid(userA.findById("E01"), 1100); // Error because item is still pending
